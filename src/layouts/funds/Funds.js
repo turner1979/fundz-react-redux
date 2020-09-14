ae7a7c01e734f0144dad396303006f38b812550e
Scott Turner <sturner1979@hotmail.com>
2020-09-14 12:50:58 +0100

    Added footer to funds view

--- src/layouts/funds/Funds.js
+++ src/layouts/funds/Funds.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import './Funds.scss';
+import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
 import ContentContainer from '../../components/contentContainer/ContentContainer';
 import Icon from '../../components/icon/Icon';
@@ -22,6 +23,7 @@ function Funds() {
           </div>
           <Icon iconClass="fas fa-plus" onClick={() => { console.log('TODO: set modal visibility') }} />
         </div>
+        <Footer />
       </ContentContainer>
     </div>
   );
